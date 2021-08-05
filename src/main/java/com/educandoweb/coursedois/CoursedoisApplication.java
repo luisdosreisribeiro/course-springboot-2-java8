992a86a88076887328d96f2de55ab10c09f3015b
Luis Ribeiro <luisalberto677@gmail.com>
2021-08-04 22:11:01 -0300

    Project created

---
+++ src/main/java/com/educandoweb/coursedois/CoursedoisApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.coursedois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursedoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursedoisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/coursedois/CoursedoisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.coursedois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursedoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
