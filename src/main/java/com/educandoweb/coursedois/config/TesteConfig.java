2a089824f79dc04e5cc9c08526caeb2408ca9464
Luis Ribeiro <luisalberto677@gmail.com>
2021-08-09 22:25:50 -0300

    Category

--- src/main/java/com/educandoweb/coursedois/config/TesteConfig.java
+++ src/main/java/com/educandoweb/coursedois/config/TesteConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.coursedois.entities.Category;
 import com.educandoweb.coursedois.entities.Order;
 import com.educandoweb.coursedois.entities.User;
 import com.educandoweb.coursedois.entities.enums.OrderStatus;
+import com.educandoweb.coursedois.repositories.CategoryRepository;
 import com.educandoweb.coursedois.repositories.OrderRepository;
 import com.educandoweb.coursedois.repositories.UserRepository;
 
@@ -22,9 +24,18 @@ public class TesteConfig  implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository CategoryRepository;
+	
+	
 
 	@Override
 	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
@@ -34,6 +45,7 @@ public class TesteConfig  implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		CategoryRepository.saveAll(Arrays.asList(cat1, cat2,cat3));
 		
 		
 	}
