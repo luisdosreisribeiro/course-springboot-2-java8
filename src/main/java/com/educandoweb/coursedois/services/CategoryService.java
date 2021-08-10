f615e7f7f49a98666f5bcf8d449d359dfe240a08
Luis Ribeiro <luisalberto677@gmail.com>
2021-08-09 22:20:52 -0300

    Categpry

---
+++ src/main/java/com/educandoweb/coursedois/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.educandoweb.coursedois.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.coursedois.entities.Category;
+import com.educandoweb.coursedois.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+
+}
